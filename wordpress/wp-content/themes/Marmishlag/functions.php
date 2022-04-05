0287158dc8f3178a6ad7786559d88986740f87da
Annie Tran <annie.tran@hetic.net>
2022-04-05 15:12:55 +0200

    add little style

--- wordpress/wp-content/themes/Marmishlag/functions.php
+++ wordpress/wp-content/themes/Marmishlag/functions.php
@@ -1,6 +1,9 @@
 <?php
 add_action('wp_enqueue_scripts' , 'custom_css_js');
+
 add_action('after_setup_theme','marmislag_theme_support');
+
+add_action('wp_enqueue_scripts', 'charger_bootstrap');
 
 function marmislag_theme_support(){
     add_theme_support('title-tag');
@@ -13,3 +16,13 @@ function custom_css_js(){
     wp_enqueue_style('style-theme2', get_template_directory_uri( __FILE__ ).'/assets/css/single-post.css');
     wp_enqueue_style('style-theme3', get_template_directory_uri( __FILE__ ).'/assets/css/navebar.css');
 }
+
+function charger_bootstrap() {
+
+    wp_enqueue_script( 'bootstrap-js', get_stylesheet_directory_uri() . 'bootstrap/js/bootstrap.min.js', array( 'jquery' ), null, true );
+    
+    wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri() . 'bootstrap/css/bootstrap.min.css' );
+    
+}
+    
+
--- wordpress/wp-content/themes/Marmishlag/index.php
+++ wordpress/wp-content/themes/Marmishlag/index.php
@@ -1,13 +1,13 @@
 <?php get_header() ?>
-    <h1>Recettes</h1>
+    <h1>Recettes Promo</h1>
     <?php if (have_posts()) : ?>  
-        <div class="">
+        <div class="idearecette">
             <?php while (have_posts()) : ?>
             <?php the_post(); ?>
-                <div class="">
+                <div class="lignerecette">
                     <?php the_category()?>
-                    <a href="<?php the_permalink()?>"> <img class="" src=<?php the_post_thumbnail(); ?> </a>
-                    <h2 class=""><?php the_title()?></h2>
+                    <a href="<?php the_permalink()?>"> <img class="imagerecette" src=<?php the_post_thumbnail(); ?> </a>
+                    <h2 class="namerecette"><?php the_title()?></h2>
                     <?php
                     if (get_post_meta(get_the_id(), 'promomarmishlag', true)) {
                     ?>
